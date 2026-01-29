af7422ea9030d117a8ed92d81a8d2cbdf286181f
designnectar03-cmyk <designnectar03@gmail.com>
2026-01-30 00:48:51 +0530

    Initial ecommerce app commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,11 @@
-import type { Config } from 'tailwindcss';
-
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      screens: {
-        'xs': '475px',
-      },
-    },
+    extend: {},
   },
-  plugins: [require("tailwind-scrollbar-hide")],
-} satisfies Config;
+  plugins: [],
+};
